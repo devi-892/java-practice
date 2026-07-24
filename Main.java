f99cd61144705135f800404097f9d4f7538d07a7
devi-892 <devikaagatahmudi2006@gmail.com>
2026-07-24 14:02:00 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+public static void main (String[] args )   
+{
+        int a=32;
+        if(a%2==0)
+        {
+            System.out.println("it is even");
+        }
+        else
+        {
+         System.out.println("odd");   
+        }
+    }
+}
